4ca80d3d13453a641a6ef27666a72f4a82509d46
TestProgger <gamersteam_dubki@mail.ru>
2021-10-08 12:32:02 +0300

    Added useAuth hook for user authorization

---
+++ frontend/src/hooks/useAuth.tsx
@@ -0,0 +1,36 @@
+import { useCallback, useState } from "react"
+import { useHistory } from "react-router-dom";
+
+
+
+export const localStorageName  = "oldy_token";
+
+export interface AuthInterface{
+    token : string  | null | undefined
+    apiEndpoint : string
+    login : ( resp_token : string ) => void
+    logout : ( ) => void 
+}
+
+export const useAuth = () : AuthInterface => {
+    const [ token , setToken ] = useState<string | null>();
+    const apiEndpoint = "http://" + window.location.host.split(":")[0] + ":5000/api";
+
+    const history = useHistory();
+    const historyLocation = history.location.pathname; 
+
+    const login = useCallback(( resp_token : string ) => {
+        setToken( resp_token );
+        localStorage.setItem( localStorageName , resp_token );
+        history.push( historyLocation === "/auth" ? "/" : historyLocation )
+    } , []);
+
+    const logout = useCallback( () => {
+        setToken( null );
+        localStorage.removeItem(localStorageName);
+        history.go(0);
+    }  , [])
+
+    return { token  , apiEndpoint , login  , logout };
+
+}
