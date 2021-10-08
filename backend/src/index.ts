e0edcacf528d894875ff076af2163819a54e3e13
TestProgger <gamersteam_dubki@mail.ru>
2021-10-08 21:13:02 +0300

    Added Validation Valies on socket.io base

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,10 @@ import * as http from 'http';
 import * as socketio from 'socket.io';
 import cors from 'cors';
 import helmet from 'helmet';
+import { createConnection, getRepository } from 'typeorm';
 
 import validator from 'validator';
+import { User } from './entity/User';
 
 const PORT = 5000;
 
@@ -21,33 +23,89 @@ const io : socketio.Server = new socketio.Server(server , {
     cors: { origin: ['http://localhost:3000'], credentials: true },
   });
 
+enum EmitStrings{
+    EMAIL_VALIDATED = 'emailValidated',
+    PASSWORD_VALIDATED = 'passwordValidated',
+    USERNAME_VALIDATED = 'usernameValidated'
+}
 
+enum EventStrings{
+    VALIDATE_EMAIL = 'validateEmail',
+    VALIDATE_PASSWORD = 'validatePassword',
+    VALIDATE_USERNAME = 'validateUsername'
+}
 
-io.on('connect' , ( socket : socketio.Socket ) => {
-    socket.on('login' , ( client : socketio.Socket ) => {
-        console.log(client);
+enum ErrorStrings{
+    
+    INVALID_EMAIL = 'Invalid Email',
+    EMAIL_ALREADY_USE = "E-Mail already in use",
+
+    INVALID_USERNAME = "Invalid Username",
+    USERNAME_ALREADY_USE = "Username alreadu in use",
+
+    INVALID_PASSWORD = "Invalid Password",
+    PASSWORDS_DIFFERENT = "Password are different",
+    SHORT_PASSWORD = "Password is to short",
+    WEAK_PASSOWRD = "Password is weak"
+
+}
+
+// Validating Values
+io.on('connect' , async ( socket : socketio.Socket ) => {
+
+    const userRepository = getRepository( User );
+
+    socket.on(EventStrings.VALIDATE_EMAIL ,  async ( email : string  ) => {
+
+        const errors : string[] = [];
+
+        if( !validator.isEmail(email) )
+        {
+            errors.push( ErrorStrings.INVALID_EMAIL);
+        }
+
+        if ( await userRepository.count( { email } ) )
+        {
+            errors.push(ErrorStrings.EMAIL_ALREADY_USE   );
+        }
+
+        socket.emit( EmitStrings.EMAIL_VALIDATED , errors.length ? { error : errors } : {} );
+        
     });
 
-    socket.on('checkEmail' , ( email : string  ) => {
-        if( validator.isEmail(email) ){
-            socket.emit('emailChecked' , {})
-        }else
+
+    socket.on( EventStrings.VALIDATE_USERNAME , async ( username : string ) => {
+        
+        const errors : string[] = [];
+
+        if ( username.length === 0 || username.length > 255)
         {
-            socket.emit('emailChecked' ,  {error : 'Error : Invalid Email'})
+            errors.push(ErrorStrings.INVALID_USERNAME );
         }
         
+        if( await userRepository.count( { username } ) )
+        {
+            errors.push( ErrorStrings.USERNAME_ALREADY_USE  );
+        }
+        
+        socket.emit( EmitStrings.USERNAME_VALIDATED , errors.length ? { error : errors } : {} );
+
+    } );
+
+    interface PasswordInterface{ password : string , confPassword : string }
+    socket.on( EventStrings.VALIDATE_PASSWORD , async( { password , confPassword } : PasswordInterface ) =>{
+        const errors : string[] =  [];
+        
+        if( password !== confPassword ){ errors.push( ErrorStrings.PASSWORDS_DIFFERENT ); }
+        if( password.length < 8 ){ errors.push( ErrorStrings.SHORT_PASSWORD ) }
+        if( !( /\d/.test( password ) && /\w/.test( password )  ) && password.length >=  8  ){ errors.push( ErrorStrings.WEAK_PASSOWRD ) }
+
+        socket.emit( EmitStrings.PASSWORD_VALIDATED , errors.length ? { error : errors } : {} );
+
     })
 
-    // socket.on('checkUsername' , ( email : string  ) => {
-    //     if( validator.isEmail(email) ){
-    //         socket.emit('emailChecked' , {})
-    //     }else
-    //     {
-    //         socket.emit('emailChecked' ,  {error : 'Error'})
-    //     }
-        
-    // })
 
 })
 
-server.listen( PORT );
+createConnection().then(() => server.listen( PORT ));
+
