323683f513d77e3c0f4f027d43ae4aabfdd48af1
TestProgger <gamersteam_dubki@mail.ru>
2021-10-08 12:37:35 +0300

    Added AuthPage

---
+++ frontend/src/pages/AuthPage/AuthPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+
+import "./AuthPage.css";
+
+export const AuthPage : FC = () => {
+    return (
+        <div className="auth-page-container">
+
+        </div>
+    )
+}
---
+++ frontend/src/pages/AuthPage/forms/LoginForm/LoginForm.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+
+
+export const LoginForm : FC = () => {
+    return (
+        <div></div>
+    )
+}
