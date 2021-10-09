89d7ea8a613b9188e50526da559541ec6c6d746f
TestProgger <gamersteam_dubki@mail.ru>
2021-10-09 11:22:45 +0300

    Added Auth and Validate enums with Emit , Error, Event strings

---
+++ frontend/src/enums/AuthEnums.ts
@@ -0,0 +1,11 @@
+export enum AuthEmit {
+    REGISTRATION = 'registraition',
+    LOGIN = 'login',
+    RESET = 'reset'
+}
+
+export enum AuthEvent {
+    REGISTERED = 'registered',
+    LOGED = 'loged',
+    RESETED = 'reseted'
+}
---
+++ frontend/src/enums/ValidateEnum.ts
@@ -0,0 +1,11 @@
+export enum ValidateEvent{
+    EMAIL_VALIDATED = 'emailValidated',
+    PASSWORD_VALIDATED = 'passwordValidated',
+    USERNAME_VALIDATED = 'usernameValidated'
+}
+
+export enum ValidateEmit{
+    VALIDATE_EMAIL = 'validateEmail',
+    VALIDATE_PASSWORD = 'validatePassword',
+    VALIDATE_USERNAME = 'validateUsername'
+}
