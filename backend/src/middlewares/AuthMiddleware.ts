5474e9c37043853f19d4f5647289e05a611b349a
TestProgger <gamersteam_dubki@mail.ru>
2021-10-10 15:43:39 +0300

    Added AuthMiddleware for Checking Authorized Users

---
+++ backend/src/middlewares/AuthMiddleware.ts
@@ -0,0 +1,31 @@
+import * as jwt from 'jsonwebtoken';
+import { UserService } from '../services/UserService';
+
+
+
+export const PROTECTED_EVENTS = [ 'profileImage' ];
+
+export const AuthMiddleware = ( jwtSecret : string , authService : UserService ) =>  async ( packet : any[] , next : Function ) => {
+    const [event ,  data ] = packet;
+
+    if( event in PROTECTED_EVENTS )
+    {
+        if( data?.token )
+        {
+            const decoded : any = jwt.verify( data.token , jwtSecret );
+            
+
+            const user = await authService.getUserByUUID(decoded.uuid);
+            packet = [packet[0] , { data : data.data , user }];
+            next();
+        }
+        else
+        {
+            next( new Error( "Invalid Token" ));
+        }
+    }
+    else
+    {
+        next()
+    }
+}
