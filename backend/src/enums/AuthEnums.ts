0ce4253ff272337421b8137d323177dcb8fe6b6e
TestProgger <gamersteam_dubki@mail.ru>
2021-10-10 15:44:51 +0300

    Updated enums added new Rows for RESET and CONFIRMATION

--- backend/src/enums/AuthEnums.ts
+++ backend/src/enums/AuthEnums.ts
@@ -1,13 +1,19 @@
 export enum AuthEvent {
     REGISTRATION = 'registraition',
     LOGIN = 'login',
-    RESET = 'reset'
+    RESET = 'reset',
+    RESET_PASSWORD = 'resetPassword',
+    CONFIRMATION = "confirmation"
+
 }
 
 export enum AuthEmiter {
     REGISTERED = 'registered',
     LOGED = 'loged',
-    RESETED = 'reseted'
+    RESETED = 'reseted',
+    CONFIRMED = "confirmed",
+    PASSWORD_RESETED = 'passwordReseted',
+    INAVLID_TOKEN = "invalidToken"
 }
 
 
@@ -16,5 +22,14 @@ export enum AuthError{
     REGISTRATION_ALREADY_IN_USE = "Username or E-Mail  already in use",
 
     LOGIN_IDENTITY_NOT_FOUND = "Identity not found",
-    LOGIN_PASSWORD_INCORRECT = "Password is incorrect"
+    LOGIN_PASSWORD_INCORRECT = "Password is incorrect",
+
+    CONFIRM_INCORRECT_CODE = "Incorrect code",
+    CONFIRM_INVALID_FORM = "Invalid form",
+
+    RESET_PASSWORD_INVALID_FORM = "Invalid form",
+    RESET_PASSWORD_PASSWORDS_DIFFERENT = "Password are different",
+
+    INVALID_TOKEN = "Invalid auth-token. Re-Auth please"
+
 }
