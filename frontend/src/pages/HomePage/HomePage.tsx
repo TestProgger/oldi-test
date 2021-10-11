010bc5932150dc99cc5c3a7f866b970fc1c3c759
TestProgger <gamersteam_dubki@mail.ru>
2021-10-11 14:34:18 +0300

    Added new page : HomePage

---
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+
+
+import './HomePage.css';
+
+export const HomePage:FC = () => {
+
+    return (
+
+        <div className=" base-container home-container">
+            
+            <h1> Home Page </h1>
+        
+        
+        </div>
+
+    )
+
+}
