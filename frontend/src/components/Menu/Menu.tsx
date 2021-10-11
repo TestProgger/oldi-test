53b221a44dfb35fc24d8fc3a52c2c2838ab872de
TestProgger <gamersteam_dubki@mail.ru>
2021-10-11 22:18:42 +0300

    Updated Menu

--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -2,10 +2,13 @@
 import './Menu.css';
 
 import burgerIcon from '../../icons/burger.png';
+import gridIcon  from '../../icons/grid.png'
 import defaultProfileImage from '../../icons/defaultProfile.png';
 import logOutIcon from '../../icons/log-out.png';
 import React, { useState } from 'react';
 import { useRef } from 'react';
+import { useContext } from 'react';
+import { AppContext } from '../../contexts/AppContext';
 
 export const Menu = () => {
 
@@ -14,29 +17,28 @@ export const Menu = () => {
     const imageRef = useRef<HTMLImageElement>(null);
 
     const [profileImage , setProfileImage ] = useState<string>('');
-
+    const { logout } = useContext(AppContext);
     const changeProfileImage = ( event : React.MouseEvent<HTMLInputElement>) => {
-        if ( inputRef.current?.files )
+        if ( inputRef.current?.files && inputRef.current.files[0] )
         {
-            setProfileImage( window.URL.createObjectURL( inputRef.current.files[0] ) )
+           const fileReader  = new FileReader();
+           fileReader.onload = () => setProfileImage( fileReader.result as string );
+           const file  = inputRef.current.files[0];
+        
+            fileReader.readAsDataURL( file );
         }
           
     }
 
     return (
-        <div className="menu">
+        <div className="menu" style = {showLogOut ?  { width : '360px' } : {} } >
 
-            {  showLogOut ? 
-                <div className="logOut">
-                    <img src={logOutIcon} alt="" />
-                </div>
-                :
-                null
-            }
+            <div className={ showLogOut ? "logOut" : "logOut d-none" } onClick = { () => { if(logout) logout() } }>
+                <img src={logOutIcon} alt="" />
+            </div>
 
-
-            <div className={ showLogOut ? "burger" : "burgerWithoutLogOut" }  onClick = { () => {} }>
-                <img src={burgerIcon} alt="" />
+            <div className={ showLogOut ? "burger" : "burgerWithoutLogOut" }  onClick = { () => setShowLogOut(!showLogOut) }>
+                <img src={showLogOut ? gridIcon : burgerIcon} alt="" />
             </div>
 
             <div className={showLogOut ?  "profileImage" : "profileWithoutLogOut" } onClick = { () => inputRef.current?.click() } >
