a05ed1f391e665604b74daf333c1babb1aab3d70
TestProgger <gamersteam_dubki@mail.ru>
2021-10-12 14:25:25 +0300

    Updated Menu

--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -1,49 +1,59 @@
 
 import './Menu.css';
 
-import burgerIcon from '../../icons/burger.png';
-import gridIcon  from '../../icons/grid.png'
+import burgerIcon from '../../icons/burger.svg';
+import gridIcon  from '../../icons/grid.svg'
 import defaultProfileImage from '../../icons/defaultProfile.png';
-import logOutIcon from '../../icons/log-out.png';
-import React, { useState } from 'react';
-import { useRef } from 'react';
+import logOutIcon from '../../icons/log-out.svg';
+import React, { useEffect, useState } from 'react';
+import { useRef , FC } from 'react';
 import { useContext } from 'react';
 import { AppContext } from '../../contexts/AppContext';
 
-export const Menu = () => {
+
+interface MenuInterface{
+    profileImage : string
+    setProfileImage :  ( ...args:any[]) => void
+}
+
+export const Menu : FC<MenuInterface> = ({ profileImage , setProfileImage }) => {
 
     const [ showLogOut , setShowLogOut ] = useState<boolean>(false);
     const inputRef = useRef<HTMLInputElement>(null);
     const imageRef = useRef<HTMLImageElement>(null);
 
-    const [profileImage , setProfileImage ] = useState<string>('');
+    // const [profileImage , setProfileImage ] = useState<string>('');
+
     const { logout } = useContext(AppContext);
-    const changeProfileImage = ( event : React.MouseEvent<HTMLInputElement>) => {
-        if ( inputRef.current?.files && inputRef.current.files[0] )
+    const changeProfileImage = ( event : React.ChangeEvent<HTMLInputElement>) => {
+        if ( event.target.files && event.target.files[0] )
         {
            const fileReader  = new FileReader();
-           fileReader.onload = () => setProfileImage( fileReader.result as string );
-           const file  = inputRef.current.files[0];
-        
-            fileReader.readAsDataURL( file );
+           fileReader.onload = () => setProfileImage( fileReader.result );
+            fileReader.readAsDataURL( event.target.files[0] );
         }
           
     }
 
     return (
-        <div className="menu" style = {showLogOut ?  { width : '360px' } : {} } >
+        <div className="menu" style = {showLogOut ?  { width : '360px' } : {} } 
+            
+            >
 
             <div className={ showLogOut ? "logOut" : "logOut d-none" } onClick = { () => { if(logout) logout() } }>
                 <img src={logOutIcon} alt="" />
             </div>
 
-            <div className={ showLogOut ? "burger" : "burgerWithoutLogOut" }  onClick = { () => setShowLogOut(!showLogOut) }>
+            <div className="burger"  onClick = { () => setShowLogOut(!showLogOut) }>
                 <img src={showLogOut ? gridIcon : burgerIcon} alt="" />
             </div>
 
-            <div className={showLogOut ?  "profileImage" : "profileWithoutLogOut" } onClick = { () => inputRef.current?.click() } >
-                <img src={ profileImage ? profileImage : defaultProfileImage } alt="" ref = {imageRef}   />
-                <input type="file" accept = "image/*" ref={inputRef} className="d-none" onClick ={ (event) => changeProfileImage(event) } />
+            <div className="profileImage" onClick = { () => inputRef.current?.click() } >
+                <div className="image">
+                    <img src={ profileImage ? profileImage : defaultProfileImage } alt="" ref = {imageRef}   />
+                </div>
+                
+                <input type="file" accept = "image/*" ref={inputRef} className="d-none" onChange ={ (event) => changeProfileImage(event) } />
             </div>
 
         </div>
